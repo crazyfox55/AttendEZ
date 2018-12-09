5a6adef35feac1a56c3cb85f2f6033bfed9ccca3
David McDermott <crazyfox343@gmail.com>
2018-12-08 22:37:53 -0600

    Initial setup

--- AttendEZ/Program.cs
+++ AttendEZ/Program.cs
@@ -19,6 +19,7 @@ namespace AttendEZ
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
                 .UseStartup<Startup>();
     }
 }
---
+++ AttendEZData/AttendEZContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace AttendEZData
+{
+    public class AttendEZContext : DbContext
+    {
+        public AttendEZContext(DbContextOptions options) :
+            base(options)
+        {
+
+        }
+
+        public DbSet<Organization> Users { get; set; }
+    }
+}
